e4efd8c4e667d778e637ec04bb8e85418aae6af4
SapaHPs <juanitomm2408@gmail.com>
2025-04-26 22:46:30 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+// Requiere los controladores
+require_once "controllers/controller.php";
+require_once "controllers/controllerUser.php";
+require_once "controllers/ControllerAuxiliar.php";
+require_once "controllers/ControllerMenu.php";
+require_once "controllers/ControllerCoordinador.php";
+require_once "controllers/ControllerComentarios.php";
+require_once "controllers/ControllerEstudiantes.php";
+require_once "controllers/ControllerReporte.php";
+
+// Requiere los modelos
+require_once "models/modelUser.php";
+require_once "models/ModelAuxiliar.php";
+require_once "models/Modelmenus.php";
+require_once "models/ModelCoordinador.php";
+require_once "models/ModelComentarios.php";
+require_once "models/ModelEstudiantes.php";
+require_once "models/ModelReporte.php";
+
+
+
+// Crear una instancia de MvcController y llamar al método plantilla
+$obj = new MvcController();
+$obj->plantilla();
+?>
